140499bfe212597d360d3bc629581a6b5bbe881b
sakamoto.m <sakamoto.m@water-cell.jp>
2019-08-07 15:00:03 +0900

    v1

---
+++ popup.js
@@ -0,0 +1,7 @@
+const but = document.getElementById('test');
+if (but) {
+  but.addEventListener('click', () => {
+    const uhuhu = document.querySelector("button")
+    console.log(uhuhu, 123)
+  });
+}
