a68489274ad2bb76d7faf43d57e8ba3f4f15cd55
sakamoto.m <sakamoto.m@water-cell.jp>
2019-08-08 14:46:51 +0900

    各種便利ライブラリ

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  printWidth: 140,
+  tabWidth: 2,
+  semi: true,
+  singleQuote: false,
+  trailingComma: "all",
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: "always",
+  parser: "typescript",
+};
