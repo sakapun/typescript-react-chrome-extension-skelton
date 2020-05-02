59aa869b01f1da8f1c6fef556d74c52924bb23e0
sakamoto.m <sakamoto.m@water-cell.jp>
2020-05-03 00:53:04 +0900

    HOCをしてみたけど、たぶん５割の理解

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,25 @@
-import { Box, Button } from "@chakra-ui/core";
+import { Box } from "@chakra-ui/core";
 import React from "react";
 import { Provider } from "./application/";
 import Index from "./presentation/container/Main";
 
-const DevWrapper = (props: any) => {
-  return process.env.NODE_ENV === "development" ? (
-    <Box height="100vh" display="flex" justifyContent="flex-end" backgroundColor="blackAlpha.100" paddingRight={8}>
-      <Index />
-    </Box>
-  ) : (
-    <Index />
-  );
-};
+function DevWrapper<T>(Component: React.ComponentType<T>) {
+  return (props: T) => {
+    return process.env.NODE_ENV === "development" ? (
+      <Box height="100vh" display="flex" justifyContent="flex-end" backgroundColor="blackAlpha.100" paddingRight={8}>
+        <Component {...props} />
+      </Box>
+    ) : (
+      <Component {...props} />
+    );
+  };
+}
+const DisplayComponent = DevWrapper(Index);
 
 const App: React.FC = () => {
   return (
     <Provider>
-      <DevWrapper />
+      <DisplayComponent />
     </Provider>
   );
 };
